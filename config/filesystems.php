7c182d02d78d370904968e7f9efc7165ac944f58
Azzizz84 <z734310000@gmail.com>
2025-05-18 00:03:41 +0300

    git push

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,7 +42,7 @@ return [
         'url' => env('APP_URL').'/storage',
         'visibility' => 'public',
         ],
- 
+  
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
